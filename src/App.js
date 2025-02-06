8cbe5fdba465e5e7d8ff0100fd58d29398c40659
GiGomieri05 <giovannidelucagomieri@hotmail.com>
2025-02-06 12:47:49 -0300

    add counter state and implement timeout updates for post titles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Component } from "react";
 
 class App extends Component {
   state = {
+    counter: 0,
     posts: [
       {
         id: 1,
@@ -17,16 +18,40 @@ class App extends Component {
       {
         id: 3,
         title: "Post 3",
-        body: "Aprendendo React",
+        body: "Neymar no Santos",
       },
     ],
   };
 
+  timeoutUpdate = null;
+
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = "Jogaço";
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 1000);
+  };
+
   render() {
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
+        <p>{counter}</p>
         {posts.map((post) => (
           <div key={post.id}>
             <h1>{post.title}</h1>
