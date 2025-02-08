3ba1822b860ed364086156c9401e2abfe08317e6
GiGomieri05 <giovannidelucagomieri@hotmail.com>
2025-02-08 19:56:17 -0300

    add tests for Button component and create Home.spec; remove outdated Home.test

---
+++ src/components/Button/button.spec.jsx
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react";
+import { Button } from ".";
+import userEvent from "@testing-library/user-event";
+
+describe("<Button />", () => {
+  it('should render the button with the text "Load More Posts"', () => {
+    render(<Button text="Load More Posts" />);
+
+    expect.assertions(1);
+
+    const button = screen.getByRole("button", { name: /load more posts/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should call function on button click", () => {
+    const fn = jest.fn();
+    render(<Button text="Load More Posts" onClick={fn} />);
+
+    const button = screen.getByRole("button", { name: /load more posts/i });
+
+    userEvent.click(button);
+
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it("should be disabled when disabled is true", () => {
+    render(<Button text="Load More Posts" disabled={true} />);
+
+    const button = screen.getByRole("button", { name: /load more posts/i });
+
+    userEvent.click(button);
+
+    expect(button).toBeDisabled();
+  });
+
+  it("should be disabled when disabled is false", () => {
+    render(<Button text="Load More Posts" disabled={false} />);
+
+    const button = screen.getByRole("button", { name: /load more posts/i });
+
+    userEvent.click(button);
+
+    expect(button).toBeEnabled();
+  });
+});
